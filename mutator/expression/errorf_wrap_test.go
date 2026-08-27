0f7187b8e16d4921c136b1d3d5adcfbb97f3e560
Jonathan Baldie <jon@jonbaldie.com>
2026-08-27 13:13:40 +0100

    perf: defer error wrap string variants

--- mutator/expression/errorf_wrap_test.go
+++ mutator/expression/errorf_wrap_test.go
@@ -1,11 +1,28 @@
 package expression
 
 import (
+	"go/ast"
+	"go/token"
+	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/quality-gates/mutago/v2/mutator"
 	"github.com/quality-gates/mutago/v2/test"
 )
+
+func BenchmarkMutatorErrorfWrapManyVerbs(b *testing.B) {
+	literal := `"` + strings.Repeat("%w ", 1000) + `"`
+	node := &ast.CallExpr{
+		Fun:  &ast.SelectorExpr{X: ast.NewIdent("fmt"), Sel: ast.NewIdent("Errorf")},
+		Args: []ast.Expr{&ast.BasicLit{Kind: token.STRING, Value: literal}},
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for range b.N {
+		runtime.KeepAlive(MutatorErrorfWrap(nil, nil, node))
+	}
+}
 
 func TestMutatorErrorfWrapRegistered(t *testing.T) {
 	if _, err := mutator.New("expression/errorf-wrap"); err != nil {
