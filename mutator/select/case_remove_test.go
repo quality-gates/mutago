b3488dc0961931bbaab4b0f8bf398c053b6102e5
Jonathan Baldie <jon@jonbaldie.com>
2026-08-27 13:12:42 +0100

    perf: share select clause snapshots

--- mutator/select/case_remove_test.go
+++ mutator/select/case_remove_test.go
@@ -1,11 +1,27 @@
 package selectmutator
 
 import (
+	"go/ast"
+	"go/token"
+	"runtime"
 	"testing"
 
 	"github.com/quality-gates/mutago/v2/mutator"
 	"github.com/quality-gates/mutago/v2/test"
 )
+
+func BenchmarkMutatorSelectCaseRemoveWideSelect(b *testing.B) {
+	clauses := make([]ast.Stmt, 1000)
+	for i := range clauses {
+		clauses[i] = &ast.CommClause{Case: token.Pos(i + 1), Comm: &ast.SendStmt{}}
+	}
+	node := &ast.SelectStmt{Body: &ast.BlockStmt{List: clauses}}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for range b.N {
+		runtime.KeepAlive(MutatorSelectCaseRemove(nil, nil, node))
+	}
+}
 
 func TestMutatorSelectCaseRemoveRegistered(t *testing.T) {
 	if _, err := mutator.New("select/case-remove"); err != nil {
